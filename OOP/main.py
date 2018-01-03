5d3654dd3e6477e3a2525c27aaa3fac12c66309e
LucasCorssac <lcorssac@hotmail.com>
2018-01-03 15:10:22 -0200

    Arranged code and added towers

--- OOP/main.py
+++ OOP/main.py
@@ -22,8 +22,8 @@ class Main:
         self._tower_list = []
 
         #UI attributes
-         150, 450, 100, 50
-        self._wave_button = 
+         
+        self._wave_button = Button(pygame.Rect(150, 450, 100, 50), "Wave 1")
         self._ui_elements_list = []
 
     def start(self):
@@ -44,9 +44,15 @@ class Main:
                 
                 #CATCHES MOUSE CLICK EVENT
                 if event.type == pygame.MOUSEBUTTONUP:
-                    tower = Tower(self._display)
-                    tower._pos = event.pos
-                    self._tower_list.append(tower)
+                    if self._wave_button.rect.collidepoint(event.pos):
+                        if not self._start_wave:
+                            self._set_start_wave()
+                        for e in self._enemie_list:
+                            e.set_active()
+                    else:
+                        tower = Tower(self._display)
+                        tower._pos = event.pos
+                        self._tower_list.append(tower)
 
             cont = 0
             for e in self._enemie_list:
@@ -72,21 +78,28 @@ class Main:
     def draw_screen(self):
         self._print_background(self._display)
         self._board.draw_board(self._display, 1)
+        
+        #DRAW ENEMIES
         for e in self._enemie_list:
             if e.is_active():
                 e.draw()
-        #draw towers
+
+        #DRAW TOWERS
         for _tower in self._tower_list:
             _tower.draw()
+
         #self._enemie_list[0].update()
         #self._enemie_list[1].update()
         #self._enemie_list[2].update()
 
-        self._wave_button.draw()
-        
-        self.button("Wave: "  + str(self._inimigo.get_level()),, Colors.GREEN, Colors.BRIGHT_GREEN)
+        #DRAW UI ELEMENTS
+
+        #DRAW "WAVE" BUTTON
+        self._wave_button.draw(self._display)
         pygame.display.update()
-        print(self._enemie_list[0].get_pos(), self._inimigo.is_active(), self._start_wave)
+
+
+        #print(self._enemie_list[0].get_pos(), self._inimigo.is_active(), self._start_wave)
         
 
     def _set_start_wave(self):
@@ -99,19 +112,7 @@ class Main:
 
     def _print_background(self, display):
         display.blit(pygame.image.load("img/fundo_jogo.png"), (0, 0))
-        display.blit(pygame.image.load("img/tower.png"),(70, 700))
-
-    def button(self, msg, x, y, w, h, ic, ac):
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
-        if x + w > mouse[0] > x and y + h > mouse[1] > y:
-           
-            if click[0] == 1 and not self._start_wave:
-                self._set_start_wave()
-                for e in self._enemie_list:
-                    e.set_active()
-        else:
-            pygame.draw.rect(self._display, ic, (x, y, w, h))
+        display.blit(pygame.image.load("img/tower.png"),(70, 700))            
 
         
 
--- OOP/td_game_ui.py
+++ OOP/td_game_ui.py
@@ -1,17 +1,30 @@
-class UI:
-    def __init__(self, rect):
-        self.rect = rect
+import pygame
+from config import Colors
 
+
+class UI:
+    def __init__(self, rect, text):
+        self.rect = rect
+        self.text = text
 
 class Button(UI):
-    def text_objects(self, text, font):
-        text_surface = font.render(text, True, (0,0,0,0))
+
+    def text_objects(self, font):
+        text_surface = font.render(self.text, True, (0,0,0,0))
         return text_surface, text_surface.get_rect()
 
     def draw(self, display):
-        pygame.draw.rect(self._display, ac, self.rect)
-        small_text = pygame.font.SysFont("comicsansms", 20)
-        text_surf, text_rect = self.text_objects(msg, small_text)
+        pygame.draw.rect(display, Colors.BRIGHT_GREEN, self.rect)
+        text_font = pygame.font.SysFont("comicsansms", 20)
+        text_surf, text_rect = self.text_objects(text_font)
+        
+        x = self.rect.left
+        y = self.rect.top
+
+        w = self.rect.width
+        h = self.rect.height
+
         text_rect.center = ((x + (w / 2)), (y + (h / 2)))
-        self._display.blit(text_surf, text_rect)
+        display.blit(text_surf, text_rect)
+        #pygame.draw.rect(display, Colors.GREEN, self.rect)
 
